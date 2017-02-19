2e3e001da0b82dd680412e648949bd0e84fa6553
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-02-18 20:56:41 -0800

    Initial Commit

---
+++ desktop/src/com/jacksonrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jacksonrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jacksonrpg.JacksonRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JacksonRPG(), config);
+	}
+}
