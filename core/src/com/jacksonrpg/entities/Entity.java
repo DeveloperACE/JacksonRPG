52f3ef50ec0bdcbf6fb192cdae656c47d58cd21b
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-03-27 11:34:24 -0700

    remove extra and/or useless stuff from Entity()

--- core/src/com/jacksonrpg/entities/Entity.java
+++ core/src/com/jacksonrpg/entities/Entity.java
@@ -2,10 +2,7 @@ package com.jacksonrpg.entities;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
 /** A simple non-movable character that can be interacted with.
@@ -15,8 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 public class Entity extends Actor {
 
     private Texture entityTexture;
-    private Animation animation;
-    private TextureAtlas animationAtlas;
 
     private Integer sectionX;
     private Integer sectionY;
@@ -26,7 +21,6 @@ public class Entity extends Actor {
     private Boolean flipEntityX;
     private Boolean flipEntityY;
 
-    float stateTime;
 
     /** Creates a new entity with the x, y, width, height, rotation, section, flipEntityX and flipEntityY properties
      *
@@ -83,90 +77,24 @@ public class Entity extends Actor {
         this(texture, x, y, width, height, rotation, 0, 0, texture.getWidth(), texture.getHeight(), flipEntityX, flipEntityY);
     }
 
-    /** Creates a new entity with the x, y, width, height, rotation and section properties
-     * Flip properties default to false (no flip)
+
+    /** Updates the Entity texture
      *
-     * @param texture The texture for the entity
-     * @param x The X coordinate of the entity
-     * @param y The Y coordinate of the entity
-     * @param width The width of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     * @param height The height of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     * @param rotation The number of degrees to rotate the entity
-     * @param sectionX The X coordinate of the start of a section of the texture to render (if the texture contains
-     *                 multiple sprites)
-     * @param sectionY The Y coordinate of the start of a section of the texture to render (if the texture contains
-     *                 multiple sprites)
-     * @param sectionWidth The width of the section of the texture to render (if the texture contains multiple sprites)
-     * @param sectionHeight The height of the section of the texture to render (if the texture contains multiple sprites)
+     * @param newTexture The new texture to update to
      */
-    public Entity(Texture texture, float x, float y, float width, float height, float rotation, int sectionX, int sectionY, int sectionWidth, int sectionHeight) {
-        this(texture, x, y, width, height, rotation, sectionX, sectionY, sectionWidth, sectionHeight, false, false);
+    public void updateTexture(Texture newTexture) {
+        entityTexture = newTexture;
     }
 
-    /** Creates a new entity with the x, y, width, height and rotation properties
-     * Section properties default to the x, y, width and height values of the entire texture
-     * Flip properties default to false (no flip)
+    /** Returns the entity texture
      *
-     * @param texture The texture for the entity
-     * @param x The X coordinate of the entity
-     * @param y The Y coordinate of the entity
-     * @param width The width of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     * @param height The height of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     * @param rotation The number of degrees to rotate the entity
+     * @returns the entity's texture
      */
-    public Entity(Texture texture, float x, float y, float width, float height, float rotation) {
-        this(texture, x, y, width, height, rotation, 0, 0, texture.getWidth(), texture.getHeight());
+    public Texture getTexture() {
+        return entityTexture;
     }
 
-    /** Creates a new entity with the x, y, width and height properties
-     * Rotation defaults to 0 degrees
-     * Section properties default to the x, y, width and height values of the entire texture
-     * Flip properties default to false (no flip)
-     *
-     * @param texture The texture for the entity
-     * @param x The X coordinate of the entity
-     * @param y The Y coordinate of the entity
-     * @param width The width of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     * @param height The height of the entity. Texture will only be scaled to these dimensions if the aspect ratio
-     *              matches that of the texture
-     */
-    public Entity(Texture texture, float x, float y, float width, float height) {
-        this(texture, x, y, width, height, 0);
-    }
 
-    /** Creates a new entity with the x and y properties
-     * Width defaults to 50 pixels
-     * Height defaults to 100 pixels
-     * Rotation defaults to 0 degrees
-     * Section properties default to the x, y, width and height values of the entire texture
-     * Flip properties default to false (no flip)
-     *
-     * @param texture The texture for the entity
-     * @param x The X coordinate of the entity
-     * @param y The Y coordinate of the entity
-     */
-    public Entity(Texture texture, float x, float y) {
-        this(texture, x, y, 50, 100);
-    }
-
-    /** Creates a new entity with the given texture
-     * X and Y default to (0, 0)
-     * Width defaults to 50 pixels
-     * Height defaults to 100 pixels
-     * Rotation defaults to 0 degrees
-     * Section properties default to the x, y, width and height values of the entire texture
-     * Flip properties default to false (no flip)
-     *
-     * @param texture The texture for the entity
-     */
-    public Entity(Texture texture) {
-        this(texture, 0, 0);
-    }
 
     /** Updates the Entity texture
      *
@@ -177,38 +105,12 @@ public class Entity extends Actor {
     }
 
 
-    public void addTextureAnimation(TextureAtlas atlas, float frameRate) {
-        animationAtlas = atlas;
-        animation = new Animation<TextureRegion>(1/frameRate, atlas.findRegions("running"), Animation.PlayMode.LOOP);
     }
 
 
-    /** Returns the current entity for inclusion in a Stage if needed
-     *
-     * @return The current Entity() object
-     */
-    public Actor getActor() {return this;}
-
-    /** Move the Entity by
-     *
-     * @param x
-     * @param y
-     */
-    public void moveBy(float x, float y) {
-        this.setX(this.getX() + x);
-        this.setY(this.getY() + y);
-    }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-
-        stateTime += Gdx.graphics.getDeltaTime();
-
-        if (animation != null) {
-            Integer currentFrameIndex = animation.getKeyFrameIndex(stateTime);
-            entityTexture = animationAtlas.getRegions().get(currentFrameIndex).getTexture();
-
-        }
 
         batch.draw(
                 entityTexture,
