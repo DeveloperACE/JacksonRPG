d554cd1ab676e6cdda0c05029ac22ff5b1f60765
Adrian Edwards <DeveloperACE@users.noreply.github.com>
2017-04-15 13:13:49 -0700

    Create Settings screen class

---
+++ core/src/com/jacksonrpg/game/screens/SettingsScreen.java
@@ -0,0 +1,46 @@
+package com.jacksonrpg.game.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Adrian on 4/15/17.
+ */
+public class SettingsScreen implements Screen {
+
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+    }
+}
