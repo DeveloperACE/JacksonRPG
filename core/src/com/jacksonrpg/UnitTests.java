cefe4e8b12f841c0ce2525f55fe8403682489417
EDWARDS <EDWAR12421@loswego.k12.or.us>
2017-05-18 08:55:27 -0700

    unit tests work now... i think

--- core/src/com/jacksonrpg/UnitTests.java
+++ core/src/com/jacksonrpg/UnitTests.java
@@ -1,7 +1,20 @@
 package com.jacksonrpg;
 
+import org.junit.jupiter.api.Test;
+
+import org.junit.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by edwar12421 on 5/10/2017.
  */
 public class UnitTests {
+
+    @Test
+    public void test1()
+    {
+        assertEquals(1, 1);
+
+    }
 }
